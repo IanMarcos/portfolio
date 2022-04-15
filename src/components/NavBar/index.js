611b233b26f382d49b6dd6565783e3afbde888ad
Ian Marcos <ian.marcos.ortiz@gmail.com>
2022-04-15 14:33:22 -0500

    NavBar created

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import { VscOutput, VscCode , VscMortarBoard, VscAccount } from 'react-icons/vsc';
+import { navigationBar } from './style.module.css';
+
+const NavigationBar = () => {
+    return (
+        <nav className={navigationBar}>
+            <ul>
+                <li><Link to="/">
+                    <VscOutput size={18}/>
+                    <p>Resumen</p>
+                </Link></li>
+                <li><Link to="/portfolio">
+                    <VscCode size={18}/>
+                    <p>Portafolio</p>
+                </Link></li>
+                <li><Link to="/knowledge">
+                    <VscMortarBoard size={18}/>
+                    <p>Estudios</p>
+                </Link></li>
+                <li><Link to="/experience">
+                    <VscAccount size={18}/>
+                    <p>Experiencia</p>
+                </Link></li>
+            </ul>
+        </nav>
+    )
+}
+export default NavigationBar;
