1ed0ee28c26b1607fddbbb64178897cd7130793b
Ian Marcos <ian.marcos.ortiz@gmail.com>
2022-05-02 20:54:18 -0500

    404 page created

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
+import { centerContent, four0four, rightSide } from './style.module.css';
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <Link to="/">Go home</Link>.
-    </main>
+    <div className={centerContent}>
+      <div className={four0four}>
+        <div>
+          <h1>404</h1>
+        </div>
+        <div className={rightSide}>
+          <h2>Esta página no existe</h2>
+          <Link to="/"><button>Inicio</button></Link>
+        </div>
+      </div>
+      <div className='top-minus-one'>
+        <pre>{`_      _      _
+ <(.)__ >(.)__ <(.)__
+   (___/  (___/  (___/ `}</pre>
+        </div>
+    </div>
   )
 }
 export default NotFoundPage;
