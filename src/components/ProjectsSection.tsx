0e6ea088bed300b87347256332eeee71f2fa3f1c
Ian Ortiz <ian.marcos.ortiz@gmail.com>
2024-01-28 17:53:28 -0500

    feat: slight changes to the ProjectsSection styles

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 import ProjectCard from './ProjectCard';
 import projectsData from 'assets/data/projects/en.json';
 import { Project } from '../types/Project';
@@ -12,7 +13,7 @@ const ProjectsSection = () => {
       <h2>Projects</h2>
       <ul>
         {projects.map((project: Project) => (
-          <li>
+          <li key={nanoid(10)}>
             <ProjectCard project={project} />
           </li>
         ))}
